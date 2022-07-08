9215721e1a1340b4a81b9d5fdd28a02115e6e87c
elias <bianchi.elias@gmail.com>
2022-07-08 12:43:21 -0300

    botones lindos

--- index.js
+++ index.js
@@ -10,34 +10,29 @@ function multiplicacion(first, second){
 function divicion(first, second){
 	return first / second
 }
-function derivacion(first, second){
-	return first / second
-}
-function integracion(first, second){
-	return first / second
-}
 
 var calculator = document.getElementById('calculator')
 
 calculator.innerHTML=`
 	<input type='text' name='input' id='input' value="0">
-	<input id="one" type="submit" value="1">
-	<input id="two" type="submit" value="2">
-	<input id="three" type="submit" value="3">
-	<input id="for" type="submit" value="4">
-	<input id="five" type="submit" value="5">
-	<input id="six" type="submit" value="6">
-	<input id="seven" type="submit" value="7">
-	<input id="eigth" type="submit" value="8">
-	<input id="nine" type="submit" value="9">
-	<input id="cero" type="submit" value="0">
-	<input id="plus" type="submit" value="+">
-	<input id="menos" type="submit" value="-">
-	<input id="divir" type="submit" value="/">
-	<input id="multiplicar" type="submit" value="*">
-	<input id="derivar" type="submit" value="der">
-	<input id="integrar" type="submit" value="int">
-	<input id="igual" type="submit" value="=">
+	<div class="botones">
+		<input id="seven" type="submit" value="7">
+		<input id="eigth" type="submit" value="8">
+		<input id="nine" type="submit" value="9">
+		<input id="for" type="submit" value="4">
+		<input id="five" type="submit" value="5">
+		<input id="six" type="submit" value="6">
+		<input id="one" type="submit" value="1">
+		<input id="two" type="submit" value="2">
+		<input id="three" type="submit" value="3">
+		<input id="cero" type="submit" value="0">
+		<input id="plus" type="submit" value="+">
+		<input id="menos" type="submit" value="-">
+		<input id="divir" type="submit" value="/">
+		<input id="multiplicar" type="submit" value="*">
+		<input id="igual" type="submit" value="=">
+		<input id="del" type="submit" value="DEL">
+	</div>
 `
 
 var input = document.getElementById('input')
@@ -58,7 +53,7 @@ window.addEventListener('load', () => {
 		if(input.value != ""){
 			operacion = "suma"
 			first = parseInt(input.value)
-			input.value=""
+			input.value="0"
 			input.focus()
 		}
 		console.log('shf');
@@ -68,7 +63,7 @@ window.addEventListener('load', () => {
 			operacion = "menos"
 			first = input.value
 			input.focus()
-			input.value=""
+			input.value="0"
 		}
 		console.log('shf');
 	})
@@ -76,7 +71,7 @@ window.addEventListener('load', () => {
 		if(input.value != ""){
 			operacion = "multiplicar"
 			first = input.value
-			input.value=""
+			input.value="0"
 			input.focus()
 		}
 		console.log('shf');
@@ -85,7 +80,7 @@ window.addEventListener('load', () => {
 		if(input.value != ""){
 			operacion = "divir"
 			first = input.value
-			input.value=""
+			input.value="0"
 			input.focus()
 		}
 		console.log('shf');
@@ -106,5 +101,40 @@ window.addEventListener('load', () => {
 				input.value = divicion(first, second)
 				break
 		}
+	})
+	document.getElementById('one').addEventListener('click', () => {
+		input.value == "0" ? input.value = "1" : input.value += "1"
+	})
+	document.getElementById('two').addEventListener('click', () => {
+		input.value == "0" ? input.value = "2" : input.value += "2"
+	})
+	document.getElementById('three').addEventListener('click', () => {
+		input.value == "0" ? input.value = "3" : input.value += "3"
+	})
+	document.getElementById('for').addEventListener('click', () => {
+		input.value == "0" ? input.value = "4" : input.value += "4"
+	})
+	document.getElementById('five').addEventListener('click', () => {
+		input.value == "0" ? input.value = "5" : input.value += "5"
+	})
+	document.getElementById('six').addEventListener('click', () => {
+		input.value == "0" ? input.value = "6" : input.value += "6"
+	})
+	document.getElementById('seven').addEventListener('click', () => {
+		input.value == "0" ? input.value = "7" : input.value += "7"
+	})
+	document.getElementById('eigth').addEventListener('click', () => {
+		input.value == "0" ? input.value = "8" : input.value += "8"
+	})
+	document.getElementById('nine').addEventListener('click', () => {
+		input.value == "0" ? input.value = "9" : input.value += "9"
+	})
+	document.getElementById('cero').addEventListener('click', () => {
+		input.value == "0" ? input.value = "0" : input.value += "0"
+	})
+	document.getElementById('del').addEventListener('click', () => {
+		input.value = "0"
+		first = 0
+		second = 0
 	})
 })
